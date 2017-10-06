6ad21d913491f36135c63c8334bb3b6fd2943277
Abe Land <abesland@gmail.com>
2017-10-06 00:03:47 -0700

    Update problem9.rb

--- lib/euler/problem9.rb
+++ lib/euler/problem9.rb
@@ -1,6 +1,6 @@
 module Problem9
   def problem9
-    (1..498).each do |a| # I'm sure there's a tigher bound but I don't really care.
+    (1..498).each do |a| # I'm sure there's a tighter bound but I don't really care.
       ((a+1)..499).each do |b| # a < b < c and a + b + c = 1000
         c = 1000 - a - b # a + b + c = 1000
         next if c <= b
